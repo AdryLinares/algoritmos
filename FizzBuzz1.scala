e15b7e2981b7cefe5c95ad2837aaf420b9ce28e8
AdryLinares <adriana8_aln@hotmail.com>
2015-04-26 18:11:32 -0500

    Agrego Tarea1

---
+++ FizzBuzz1.scala
@@ -0,0 +1,12 @@
+Object FizzBuzz{
+def main (args: Array){
+		for (i <- 1 to 100) {
+  			if ( i % 3 == 0 && i% 5 == 0) 
+    			println(i +" = FizzBuzz")
+  			else if (i % 3 == 0) 
+    			println(i +" = Fizz")
+  			else if (i % 5 == 0) 
+    			println(i +" = Buzz")
+					  		}
+					   }
+				}
